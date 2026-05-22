f7b9e8abbffbbc3b08b3c6d9e53f3e2919947213
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-23 00:05:30 +0600

    Working on Documentation

--- templates/base-template/application/starter/src/config/app-adapter.tsx
+++ templates/base-template/application/starter/src/config/app-adapter.tsx
@@ -1,19 +1,27 @@
-import type {UIAdapterData} from "mfront-ui";
+import {Loader, ToastBox, type UIAdapterData} from "mfront-ui";
 import {MAdapter, type UINode} from "mfront";
+import {DefaultUIImplementation} from "mfront-default-ui";
+import {LoaderPinwheel} from "lucide-react";
 
 export default class AppAdapter extends MAdapter {
     setCentralUI(): UINode {
         return (
-            <></>
-        );
+            <>
+                <ToastBox/>
+            </>
+        )
     }
 
     setSuspense(): UINode {
-        return (<div>Loading page...</div>)
+        return (
+            <div className="flex min-h-screen w-full items-center justify-center">
+                <Loader size="xl" speed={3} icon={LoaderPinwheel}/>
+            </div>
+        )
     }
 
     setUIAdapter(): UIAdapterData {
-        return null;
+        return DefaultUIImplementation;
     }
 
 }
